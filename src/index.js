167f9336761f21525fdf62af1bec74ae6e397f87
dhruv patel <drpatel9054@gmail.com>
2019-12-24 22:50:01 +0530

    new localstorage for cart items added in our app using react-persist

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import { Provider} from 'react-redux';
+import {persistGate} from 'redux-persist/integration/react';
 import store from './redux/store';
+import persistor from './redux/store';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(  
                   <Provider store={store}>
 	              <BrowserRouter>
+	              <persistGate persistor={persistor}>
 	              <App />
-	            </BrowserRouter>
-	            </Provider>,
+	              </persistGate>
+	              </BrowserRouter>
+	              </Provider>,
 	            document.getElementById('root')
 	           );
 
